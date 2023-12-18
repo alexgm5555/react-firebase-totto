26ae7aa9d59df4429d5c92c5192c1f713592281c
alexgm5555 <alexgm5555@hotmail.com>
2023-12-17 22:55:36 -0500

    fix: cleanCode

--- src/componets/index.tsx
+++ src/componets/index.tsx
@@ -1,5 +1,4 @@
 export { LoginForm } from './LoginFom';
 export { CameraCapture } from './CameraCapture';
 export { GaleryImages } from './GaleryImages';
-export { PictureAlbum } from './PictureAlbum';
 export { Loading } from './Loading';
